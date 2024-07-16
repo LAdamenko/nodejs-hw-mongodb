9d0f65518b7e52dbb17e3cc498d5ab8c4e219493
LAdamenko <148965340+LAdamenko@users.noreply.github.com>
2024-07-16 15:35:25 +0300

    Add completed homework for hw2-mongodb

---
+++ src/db/models/contact.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const contactsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+const ContactsCollection = mongoose.model('Contacts', contactsSchema);
+
+export { ContactsCollection };
