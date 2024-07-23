9a8cae983e1e3e839c5b108e3683b6c35f51f720
LAdamenko <148965340+LAdamenko@users.noreply.github.com>
2024-07-23 14:47:24 +0300

    Add completed homework for hw3-crud

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import contactsRouter from './routers/contacts.js';
 import dotenv from "dotenv";
-import { getAllContacts, getContactById } from './services/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 dotenv.config();
 
@@ -20,45 +22,13 @@ export function setupServer() {
 
   app.use(cors());
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+  app.use(express.json());
 
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data: contacts,
-    });
-  });
+  app.use(contactsRouter);
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    try {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+  app.use('*', notFoundHandler);
 
-	if (!contact) {
-	  res.status(404).json({
-		  message: 'Contact not found',
-	  });
-	  return;
-      }
-      res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).send('Not found');
-  });
-
-  app.use((error, req, res, next) => {
-    res.status(500).send('Internal Server Error');
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
